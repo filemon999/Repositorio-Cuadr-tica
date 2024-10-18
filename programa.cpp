6ee04204daa8a4b0d7b2839215cfdc4cbf5d5943
Miguel <gamm081007hgtrsga9@cbtis217.edu.mx>
2024-10-17 23:13:12 -0600

    Programa con un solo comentario al inicio de este

--- programa.cpp
+++ programa.cpp
@@ -1,17 +1,13 @@
-//Bibliotecas
+//Este programa pedira los datos A, B y C al usuario y calculara e imprimira las raices x1 y x2 de una ecuación cuadrática
 #include "stdio.h"
 #include "iostream"
 #include "math.h"
 using namespace std;
 int main ()
 {
-    //Declarar las variables
     double a,b,c,x1,x2;
-    //Obtener los datos ocupados
     cin>>a>>b>>c;
-    //Hacer las operaciones, una donde se sume y la otra donde se reste
     x1=((-1*b)+sqrt(pow(b,2)-4*a*c))/(2*a);
     x2=((-1*b)-sqrt(pow(b,2)-4*a*c))/(2*a);
-    //Imprimir los resultados
     cout<<x1<<" "<<x2;
 }
